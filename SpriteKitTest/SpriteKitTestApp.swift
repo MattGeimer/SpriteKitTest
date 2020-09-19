f55400a07c20f8ffe39d445212f7090c39374a11
Matt Geimer <mgeimer2@illinois.edu>
2020-09-19 15:18:43 -0500

    Initial Commit

---
+++ SpriteKitTest/SpriteKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKitTestApp.swift
+//  SpriteKitTest
+//
+//  Created by Matthew Geimer on 9/19/20.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
